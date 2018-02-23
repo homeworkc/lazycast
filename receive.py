3168dedca7a98be20edaa5c2f3898fd4e8c9b55e
homeworkc <qw3kqt@gmail.com>
2018-02-23 16:39:12 +0000

    modified:   d2.py 	modified:   receive.py 	modified:   udhcpd.conf

--- receive.py
+++ receive.py
@@ -5,11 +5,13 @@ from struct import *
 
 address = ('0.0.0.0', 1028)
 rtpsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+rtpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 rtpsock.bind(address)
+file=open("win10data.ts","w")
 while 1:
-    data=(rtpsock.recv(2000))
-    tsdata=data[8:len(data)]
-    print ord(tsdata[0:1])
-#    print binascii.hexlify(data)
-
+	data=(rtpsock.recv(2000))
+	tsdata=data[12:len(data)]
+	file.write(tsdata)
+	print data[0:12]
+file.close()
 
