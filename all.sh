9ecfdb91f0ddd5c0c9746b9fce3dd1f4a82cf344
homeworkc <36410112+homeworkc@users.noreply.github.com>
2020-04-11 10:16:04 -0400

    modified:   all.sh

--- all.sh
+++ all.sh
@@ -11,6 +11,10 @@
 #################################################################################
 while :
 do
+	p2pdevinterface=$(sudo wpa_cli interface | grep -E "p2p-dev" | tail -1)
+	wlaninterface=$(echo $p2pdevinterface | cut -c1-8 --complement)
+	echo $p2pdevinterface
+	echo $wlaninterface
 	ain="$(sudo wpa_cli interface)"
 	echo "${ain}"
 	if [ `echo "${ain}" | grep -c "p2p-wl"` -gt 0 ] 
@@ -18,14 +22,14 @@ do
 		echo "already on"
 
 	else
-		sudo wpa_cli p2p_find type=progessive
-		sudo wpa_cli set device_name lazycast_on_"$(uname -n)"
-		sudo wpa_cli set device_type 7-0050F204-1
-		sudo wpa_cli set p2p_go_ht40 1
-		sudo wpa_cli wfd_subelem_set 0 000600111c44012c
-		sudo wpa_cli wfd_subelem_set 1 0006000000000000
-		sudo wpa_cli wfd_subelem_set 6 000700000000000000
-		perentry="$(sudo wpa_cli list_networks | grep "\[DISABLED\]\[P2P-PERSISTENT\]" | tail -1)"
+		sudo wpa_cli -i$p2pdevinterface p2p_find type=progessive
+		sudo wpa_cli -i$p2pdevinterface set device_name lazycast_on_"$(uname -n)"
+		sudo wpa_cli -i$p2pdevinterface set device_type 7-0050F204-1
+		sudo wpa_cli -i$p2pdevinterface set p2p_go_ht40 1
+		sudo wpa_cli -i$p2pdevinterface wfd_subelem_set 0 000600111c44012c
+		sudo wpa_cli -i$p2pdevinterface wfd_subelem_set 1 0006000000000000
+		sudo wpa_cli -i$p2pdevinterface wfd_subelem_set 6 000700000000000000
+		perentry="$(sudo wpa_cli -i$p2pdevinterface list_networks | grep "\[DISABLED\]\[P2P-PERSISTENT\]" | tail -1)"
 		echo "${perentry}"
 		if [ `echo "${perentry}" | grep -c "P2P-PERSISTENT"`  -gt 0 ] 
 		then
@@ -35,12 +39,19 @@ do
 			perstr=""
 		fi
 		echo "${perstr}"
+		echo "${p2pdevinterface}"
+		wlanfreq=$(sudo wpa_cli -i$wlaninterface status | grep "freq")
+		if [ "$wlanfreq" != "" ]
+		then	
+			echo $wlaninterface": "$wlanfreq
+			echo "Setting up wifi p2p with "$wlanfreq
+		fi
 		while [ `echo "${ain}" | grep -c "p2p-wl"`  -lt 1 ] 
 		do
 			while [ `echo "${ain}" | grep -c "p2p-wl"`  -lt 1 ]
 			do
-				sudo wpa_cli p2p_group_add persistent$perstr
-				#sudo wpa_cli p2p_group_add persistent$perstr freq=2
+				sudo wpa_cli p2p_group_add -i$p2pdevinterface persistent$perstr $wlanfreq
+				#sudo wpa_cli p2p_group_add -i$p2pdevinterface persistent$perstr freq=2
 				sleep 2
 				ain="$(sudo wpa_cli interface)"
 				echo "$ain"
@@ -75,23 +86,26 @@ do
 		then
 			break
 		fi
-		wlaninterface=$(sudo wpa_cli interface | grep -Ev "p2p|interfaces")
+		
 		wlanfreq=$(sudo wpa_cli -i$wlaninterface status | grep "freq")
 		p2pfreq=$(sudo wpa_cli -i$p2pinterface status | grep "freq")
-		if [ "$wlanfreq" != "$p2pfreq" ] 
-		then
-			echo "The display is disconnected since "$wlaninterface" changes from "$p2pfreq" to "$wlanfreq
-			echo "To disable WLAN roaming, run: sudo killall -STOP NetworkManager"
-			echo "You can re-enable roaming afterwards by running: sudo killall -CONT NetworkManager"
-			sudo wpa_cli -i$p2pinterface p2p_group_remove $p2pinterface
-			while :
-			do
-				if [ `sudo wpa_cli interface | grep -c "p2p-wl"` == 0 ] 
-				then
-					break
-				fi
-			done
-			break
+		if [ "$wlanfreq" != "" ]
+		then		
+			if [ "$wlanfreq" != "$p2pfreq" ] 
+			then
+				echo "The display is disconnected since "$wlaninterface" changes from "$p2pfreq" to "$wlanfreq
+				echo "To disable WLAN roaming, run: sudo killall -STOP NetworkManager"
+				echo "You can re-enable roaming afterwards by running: sudo killall -CONT NetworkManager"
+				sudo wpa_cli -i$p2pinterface p2p_group_remove $p2pinterface
+				while :
+				do
+					if [ `sudo wpa_cli interface | grep -c "p2p-wl"` == 0 ] 
+					then
+						break
+					fi
+				done
+				break
+			fi
 		fi
 
 	done
